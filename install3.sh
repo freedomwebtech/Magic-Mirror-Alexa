ae39a83e19475d7ad6e1f0709b18ee741867ef88
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-08-16 16:36:48 +0530

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -51,7 +51,4 @@ cmake -DCMAKE_PREFIX_PATH=/home/pi/sdk_folder/sdk-install \
 
 make
 
-cd /home/pi/sdk_folder/ss-build
- PA_ALSA_PLUGHW=1 ./modules/Alexa/SampleApp/src/SampleApp -C \
- /home/pi/sdk_folder/sdk-build/Integration/AlexaClientSDKConfig.json -C \
- /home/pi/sdk_folder/alexa-smart-screen-sdk/modules/GUI/config/SmartScreenSDKConfig.json -L INFO
+
