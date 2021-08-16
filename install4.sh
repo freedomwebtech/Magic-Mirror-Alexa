17ee5b8078b88a8d4aa53849dac9321b73c9f393
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-08-16 16:36:28 +0530

    Add files via upload

---
+++ install4.sh
@@ -0,0 +1,5 @@
+cd /home/pi/sdk_folder/ss-build
+ PA_ALSA_PLUGHW=1 ./modules/Alexa/SampleApp/src/SampleApp -C \
+ /home/pi/sdk_folder/sdk-build/Integration/AlexaClientSDKConfig.json -C \
+ /home/pi/sdk_folder/alexa-smart-screen-sdk/modules/GUI/config/SmartScreenSDKConfig.json -L INFO
+
