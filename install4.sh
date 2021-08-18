1ee658dc6cad33938471f354221e0150c000bdc0
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-08-18 09:15:05 +0530

    Delete install4.sh

--- install4.sh
+++
@@ -1,5 +0,0 @@
-cd /home/pi/sdk_folder/ss-build
- PA_ALSA_PLUGHW=1 ./modules/Alexa/SampleApp/src/SampleApp -C \
- /home/pi/sdk_folder/sdk-build/Integration/AlexaClientSDKConfig.json -C \
- /home/pi/sdk_folder/alexa-smart-screen-sdk/modules/GUI/config/SmartScreenSDKConfig.json -L INFO
-
